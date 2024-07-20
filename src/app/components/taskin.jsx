cac35428c5acbed614f50f555af82fffa0beb9e7
Chay <141809834+Chay-c@users.noreply.github.com>
2024-07-20 15:30:14 +0700

    Delete src/app/components directory

--- src/app/components/taskin.jsx
+++
@@ -1,9 +0,0 @@
-function TaskIn(){
-  return(
-    <div className="d-flex gap-1">
-          <input className="form-control" placeholder="Insert a task here.."></input>
-          <button className="btn btn-primary">Add</button>
-    </div>
-  )
-}
-export default TaskIn
